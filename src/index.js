f1d1837d3613c835b3e1db42f201f26f587e605f
enhao25 <larloller@gmail.com>
2019-12-28 11:32:09 +0800

    Finish assignment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
+import { searchRobots, requestRobots } from './reducers'
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+// Store can be passed down as a prop
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
